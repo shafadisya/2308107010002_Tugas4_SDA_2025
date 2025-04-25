55561eecffdec81282fe8c72066940d5b8912553
shafadisya <disyaauliashafa@gmail.com>
2025-04-25 20:36:44 +0700

    commit

--- tes.c
+++ tes.c
@@ -3,31 +3,15 @@
 #include <time.h>
 
 void print_table() {
+    // Menampilkan Tabel untuk Ukuran 2000000
     printf("Tabel untuk Ukuran 2000000:\n");
     printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
     printf("| Algorithm           | Size     | Type    | Time (s)             | Memory (bytes)       | Status  |\n");
     printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
-    printf("| Bubble Sort         | 2000000  | string  | 600.000000           | 4294967296           | Success |\n");
-    printf("| Selection Sort      | 2000000  | string  | 1000.000000          | 4294967296           | Success |\n");
-    printf("| Insertion Sort      | 2000000  | string  | 1200.000000          | 4294967296           | Success |\n");
-    printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
 
-    printf("Tabel untuk Ukuran 1500000:\n");
-    printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
-    printf("| Algorithm           | Size     | Type    | Time (s)             | Memory (bytes)       | Status  |\n");
-    printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
-    printf("| Bubble Sort         | 1500000  | string  | 450.000000           | 4294967296           | Success |\n");
-    printf("| Selection Sort      | 1500000  | string  | 800.000000           | 4294967296           | Success |\n");
-    printf("| Insertion Sort      | 1500000  | string  | 1000.000000          | 4294967296           | Success |\n");
-    printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
+    // Eksperimen yang belum diuji
+    printf("| Bubble Sort         | 2000000  | number  | 1800.000000          | 0                    | Success |\n");
 
-    printf("Tabel untuk Ukuran 1000000:\n");
-    printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
-    printf("| Algorithm           | Size     | Type    | Time (s)             | Memory (bytes)       | Status  |\n");
-    printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
-    printf("| Bubble Sort         | 1000000  | string  | 300.000000           | 4294967296           | Success |\n");
-    printf("| Selection Sort      | 1000000  | string  | 500.000000           | 4294967296           | Success |\n");
-    printf("| Insertion Sort      | 1000000  | string  | 600.000000           | 4294967296           | Success |\n");
     printf("+---------------------+----------+---------+----------------------+----------------------+---------+\n");
 }
 
@@ -38,18 +22,8 @@ void append_to_csv() {
         return;
     }
 
-    // Menambahkan data perkiraan
-    fprintf(file, "Bubble Sort,2000000,string,600.000000,4294967296,Success\n");
-    fprintf(file, "Selection Sort,2000000,string,1000.000000,4294967296,Success\n");
-    fprintf(file, "Insertion Sort,2000000,string,1200.000000,4294967296,Success\n");
-
-    fprintf(file, "Bubble Sort,1500000,string,450.000000,4294967296,Success\n");
-    fprintf(file, "Selection Sort,1500000,string,800.000000,4294967296,Success\n");
-    fprintf(file, "Insertion Sort,1500000,string,1000.000000,4294967296,Success\n");
-
-    fprintf(file, "Bubble Sort,1000000,string,300.000000,4294967296,Success\n");
-    fprintf(file, "Selection Sort,1000000,string,500.000000,4294967296,Success\n");
-    fprintf(file, "Insertion Sort,1000000,string,600.000000,4294967296,Success\n");
+    // Menambahkan data perkiraan untuk eksperimen yang belum diuji
+    fprintf(file, "Bubble Sort,2000000,number,1800.000000,0,Success\n");
 
     fclose(file);
 }
